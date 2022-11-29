620a5a5e6e2fb96d2db81053aa25f7d0133fbd94
AGauchat <agus123456@hotmail.com>
2022-11-29 15:37:41 -0300

    Flow example

--- cmd/api/handlers/design_patterns.go
+++ cmd/api/handlers/design_patterns.go
@@ -1,1 +1,26 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DesignPatternsHandler struct {
+	service DesignPatternGetter
+}
+
+func NewDesignPatternsHandler(service DesignPatternGetter) DesignPatternsHandler {
+	return DesignPatternsHandler{
+		service: service,
+	}
+}
+
+func (s DesignPatternsHandler) GetPatternByID(c *gin.Context) {
+	// aca la key la deberiamos obtener como un parametro de la url
+	response := s.service.GetPattern("some-key")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": response.Name,
+	})
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,2 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/waydevs/sections-api/cmd/api/handlers"
+	"github.com/waydevs/sections-api/internal/designpatters"
+	"github.com/waydevs/sections-api/internal/platform/repository"
+)
+
+func main() {
+	r := gin.Default()
+
+	dbConn, err := repository.NewRepository()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	dbDesignPattern := repository.NewDesignPattern(dbConn)
+
+	designPatternService := designpatters.NewDesignPattersService(dbDesignPattern)
+
+	r = handlers.DesignPatternRoutes(r, designPatternService)
+
+	r.Run()
+}
